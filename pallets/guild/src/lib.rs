a03af9e232d133121afdc4962ac5dbe72e44e954
PopcornPaws <lipschitz77@gmail.com>
2022-07-13 13:22:41 +0200

    Basic guild function calls implemented.

--- pallets/guild/src/lib.rs
+++ pallets/guild/src/lib.rs
@@ -2,39 +2,78 @@
 
 pub use pallet::*;
 
-
 #[frame_support::pallet]
 pub mod pallet {
 	use frame_support::pallet_prelude::*;
+	use frame_system::pallet_prelude::*;
 	use sp_std::vec::Vec as SpVec;
-	//use frame_system::pallet_prelude::*;
-	
+
 	#[derive(Encode, Decode, Clone, TypeInfo)]
 	pub struct Guild<AccountId> {
 		owner: AccountId,
 		members: SpVec<AccountId>,
 	}
 
+	type GuildId = u64;
+
 	#[pallet::storage]
-	pub(super) type Guilds<T: Config> = 
-		StorageMap<_, Blake2_128Concat, u64, Guild<T::AccountId>, OptionQuery>;
+	pub(super) type Guilds<T: Config> =
+		StorageMap<_, Blake2_128Concat, GuildId, Guild<T::AccountId>, OptionQuery>;
 
 	#[pallet::config]
 	pub trait Config: frame_system::Config {
 		type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
-		type CreateOrigin: EnsureOrigin<Self::Origin>;
-		type JoinOrigin: EnsureOrigin<Self::Origin>;
-		type MaxMembers: Get<u32>;
 	}
 
 	#[pallet::event]
+	#[pallet::generate_deposit(pub(super) fn deposit_event)]
 	pub enum Event<T: Config> {
-		GuildCreated,
-		GuildJoined,
+		GuildCreated(T::AccountId, GuildId),
+		GuildJoined(T::AccountId, GuildId),
+	}
+
+	#[pallet::error]
+	pub enum Error<T> {
+		GuildAlreadyExists,
+		GuildDoesNotExist,
+		SignerAlreadyJoined,
 	}
 
 	#[pallet::pallet]
 	#[pallet::without_storage_info]
 	#[pallet::generate_store(pub(super) trait Store)]
 	pub struct Pallet<T>(_);
+
+	#[pallet::call]
+	impl<T: Config> Pallet<T> {
+		#[pallet::weight(10_000)]
+		pub fn create_guild(origin: OriginFor<T>, guild_id: GuildId) -> DispatchResult {
+			let sender = ensure_signed(origin)?;
+			ensure!(!Guilds::<T>::contains_key(&guild_id), Error::<T>::GuildAlreadyExists);
+			let guild = Guild { owner: sender.clone(), members: SpVec::new() };
+			Guilds::<T>::insert(&guild_id, &guild);
+			Self::deposit_event(Event::GuildCreated(sender, guild_id));
+			Ok(())
+		}
+
+		#[pallet::weight(50_000_000)]
+		pub fn join_guild(origin: OriginFor<T>, guild_id: GuildId) -> DispatchResult {
+			let sender = ensure_signed(origin)?;
+			Guilds::<T>::try_mutate(&guild_id, |value| {
+				if let Some(guild) = value {
+					guild
+						.members
+						.binary_search(&sender)
+						.ok()
+						.ok_or(Error::<T>::SignerAlreadyJoined)?;
+					guild.members.push(sender.clone());
+					Self::deposit_event(Event::GuildJoined(sender, guild_id));
+					Ok::<(), DispatchError>(())
+				} else {
+					Err(Error::<T>::GuildDoesNotExist.into())
+				}
+			})?;
+			Ok(())
+		}
+	}
 }
