96174cad453afab8ef23eb53db4c5166016f8e85
PopcornPaws <lipschitz77@gmail.com>
2022-07-13 12:44:48 +0200

    It compiles.

--- pallets/guild/src/lib.rs
+++ pallets/guild/src/lib.rs
@@ -6,17 +6,25 @@ pub use pallet::*;
 #[frame_support::pallet]
 pub mod pallet {
 	use frame_support::pallet_prelude::*;
+	use sp_std::vec::Vec as SpVec;
 	//use frame_system::pallet_prelude::*;
 	
-	#[derive(Encode, Decode)]
-	struct Guild {
+	#[derive(Encode, Decode, Clone, TypeInfo)]
+	pub struct Guild<AccountId> {
 		owner: AccountId,
-		members: BoundedVec<AccountId, MaxLen>,
+		members: SpVec<AccountId>,
 	}
+
+	#[pallet::storage]
+	pub(super) type Guilds<T: Config> = 
+		StorageMap<_, Blake2_128Concat, u64, Guild<T::AccountId>, OptionQuery>;
 
 	#[pallet::config]
 	pub trait Config: frame_system::Config {
 		type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
+		type CreateOrigin: EnsureOrigin<Self::Origin>;
+		type JoinOrigin: EnsureOrigin<Self::Origin>;
+		type MaxMembers: Get<u32>;
 	}
 
 	#[pallet::event]
