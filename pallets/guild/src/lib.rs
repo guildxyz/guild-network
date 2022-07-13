dc94f42d81f763ae3e367cfc410402015ef93e47
PopcornPaws <lipschitz77@gmail.com>
2022-07-13 14:18:32 +0200

    Testing guild pallet.

--- pallets/guild/src/lib.rs
+++ pallets/guild/src/lib.rs
@@ -10,13 +10,14 @@ pub mod pallet {
 
 	#[derive(Encode, Decode, Clone, TypeInfo)]
 	pub struct Guild<AccountId> {
-		owner: AccountId,
-		members: SpVec<AccountId>,
+		pub owner: AccountId,
+		pub members: SpVec<AccountId>,
 	}
 
 	type GuildId = u64;
 
 	#[pallet::storage]
+	#[pallet::getter(fn guilds)]
 	pub(super) type Guilds<T: Config> =
 		StorageMap<_, Blake2_128Concat, GuildId, Guild<T::AccountId>, OptionQuery>;
 
@@ -75,5 +76,79 @@ pub mod pallet {
 			})?;
 			Ok(())
 		}
+	}
+}
+
+#[cfg(test)]
+mod test {
+	use super::*;
+	use crate as pallet_guild;
+
+	use sp_core::H256;
+	use sp_runtime::{
+		testing::Header,
+		traits::{BlakeTwo256, IdentityLookup},
+	};
+
+	use frame_support::{
+		assert_noop, assert_ok,
+		traits::{ConstU32, ConstU64, StorageVersion},
+	};
+
+	type UncheckedExtrinsic = frame_system::mocking::MockUncheckedExtrinsic<Test>;
+	type Block = frame_system::mocking::MockBlock<Test>;
+
+	frame_support::construct_runtime!(
+		pub enum Test where
+			Block = Block,
+			NodeBlock = Block,
+			UncheckedExtrinsic = UncheckedExtrinsic,
+		{
+			System: frame_system,
+			Guild: pallet_guild,
+		}
+	);
+
+	impl frame_system::Config for Test {
+		type BaseCallFilter = frame_support::traits::Everything;
+		type BlockWeights = ();
+		type BlockLength = ();
+		type DbWeight = ();
+		type Origin = Origin;
+		type Index = u64;
+		type BlockNumber = u64;
+		type Hash = H256;
+		type Call = Call;
+		type Hashing = BlakeTwo256;
+		type AccountId = u64;
+		type Lookup = IdentityLookup<Self::AccountId>;
+		type Header = Header;
+		type Event = Event;
+		type BlockHashCount = ConstU64<250>;
+		type Version = ();
+		type PalletInfo = PalletInfo;
+		type AccountData = ();
+		type OnNewAccount = ();
+		type OnKilledAccount = ();
+		type SystemWeightInfo = ();
+		type SS58Prefix = ();
+		type OnSetCode = ();
+		type MaxConsumers = ConstU32<16>;
+	}
+
+	impl Config for Test {
+		type Event = Event;
+	}
+
+	#[test]
+	fn guild_interactions_work() {
+		let mut ext: sp_io::TestExternalities =
+			frame_system::GenesisConfig::default().build_storage::<Test>().unwrap().into();
+		ext.execute_with(|| {
+			assert_ok!(Guild::create_guild(Origin::signed(4), 444));
+			assert!(Guild::guilds(444).is_some());
+			assert_ok!(Guild::join_guild(Origin::signed(4), 444));
+			assert_eq!(Guild::guilds(444).unwrap().members.len(), 1);
+		});
 	}
 }
