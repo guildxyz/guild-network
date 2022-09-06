566ae1e8a2bc0ca5a24f05b2ebf15276aaed1729
PopcornPaws <lipschitz77@gmail.com>
2022-09-06 12:07:43 +0200

    Update start script.

--- start.sh
+++ start.sh
@@ -1,37 +1,37 @@
 #!/bin/sh
 
-if [ $1 = "base" ]; then
+if [ $1 = "boot" ]; then
 	./target/release/node-template purge-chain \
-		--base-path /tmp/$2 \
+		--base-path /tmp/mynode \
 		--chain local -y
 	
 	./target/release/node-template \
-		--base-path /tmp/$2 \
+		--base-path /tmp/mynode \
 		--chain local \
 		--alice \
 		--port 30333 \
-		--ws-port 9945 \
+		--ws-port 9944 \
 		--rpc-port 9933 \
 		--node-key 0000000000000000000000000000000000000000000000000000000000000001 \
-		--telemetry-url "wss://telemetry.polkadot.io/submit/ 0" \
-		--validator
-elif [ $1 = "child" ]; then
+		--rpc-external \
+		--ws-external \
+		--rpc-cors=all
+elif [ $1 = "node" ]; then
 	./target/release/node-template purge-chain \
-		--base-path /tmp/$2 \
+		--base-path /tmp/mynode \
 		--chain local -y
 	
 	./target/release/node-template \
-		--base-path /tmp/$2 \
+		--base-path /tmp/mynode \
 		--chain local \
-		--bob \
+		--$2 \
 		--port 30334 \
-		--ws-port 9946 \
-		--rpc-port 9934 \
-		--telemetry-url "wss://telemetry.polkadot.io/submit/ 0" \
-		--validator \
-		--bootnodes /ip4/127.0.0.1/tcp/30333/p2p/12D3KooWEyoppNCUx8Yx66oV9fJnriXwCcXwDDUA2kj6vnc6iDEp
-elif [ $1 = "dev" ]; then
-	./target/release/node-template --dev
+		--ws-port 9944 \
+		--rpc-port 9933 \
+		--bootnodes /ip4/$3/tcp/30333/$4 \
+		--rpc-external \
+		--ws-external \
+		--rpc-cors=all
 elif [ $1 = "clean" ]; then
 	./target/release/node-template purge-chain --base-path /tmp/$2 -y
 elif [ $1 = "benchmark" ]; then
