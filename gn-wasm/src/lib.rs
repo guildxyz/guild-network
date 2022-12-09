f5ded998e1ba7d53d4bcc41f8889fd45615726e7
PopcornPaws <lipschitz77@gmail.com>
2022-12-09 11:48:45 +0100

    Updated wasm query tests, moved them to a separate test module.

--- gn-wasm/src/lib.rs
+++ gn-wasm/src/lib.rs
@@ -2,60 +2,6 @@ use gn_client::queries::{self, GuildFilter};
 use gn_client::Api;
 use gn_common::{pad::pad_to_32_bytes, GuildName};
 use wasm_bindgen::prelude::*;
-use wasm_bindgen_test::*;
-
-const URL: &str = "ws://127.0.0.1:9944";
-
-wasm_bindgen_test::wasm_bindgen_test_configure!(run_in_browser);
-
-fn init_tracing() {
-    console_error_panic_hook::set_once();
-    tracing_wasm::set_as_global_default();
-}
-
-#[wasm_bindgen_test]
-async fn test_query_chain() {
-    init_tracing();
-
-    let api = Api::from_url(URL).await.unwrap();
-
-    let chain = api.rpc().system_chain().await.unwrap();
-
-    assert_eq!(chain, "Development");
-}
-
-#[wasm_bindgen_test]
-async fn test_query_members() {
-    let guild = "myguild".to_string();
-    let role = "".to_string();
-
-    let members_js = query_members(guild, role, URL.to_string()).await.unwrap();
-    let members_vec: Vec<gn_client::AccountId> = members_js.into_serde().unwrap();
-
-    assert_eq!(members_vec.len(), 6);
-}
-
-#[wasm_bindgen_test]
-async fn test_query_guilds() {
-    let guild_name = "".to_string();
-    let guilds = query_guilds(guild_name, URL.to_string()).await.unwrap();
-    let guilds_vec: Vec<gn_client::data::GuildData> = guilds.into_serde().unwrap();
-
-    assert_eq!(guilds_vec.len(), 2);
-}
-
-#[wasm_bindgen_test]
-async fn test_query_requirements() {
-    let guild_name = "myguild".to_string();
-    let role_name = "myrole".to_string();
-    let requirements_js = query_requirements(guild_name, role_name, URL.to_string())
-        .await
-        .unwrap();
-    let requirements: gn_common::requirements::RequirementsWithLogic =
-        requirements_js.into_serde().unwrap();
-
-    assert_eq!(requirements.logic, "0");
-}
 
 #[wasm_bindgen(js_name = "queryMembers")]
 pub async fn query_members(guild: String, role: String, url: String) -> Result<JsValue, JsValue> {
@@ -125,5 +71,65 @@ pub async fn query_requirements(
             .map_err(|e| JsValue::from(e.to_string()))?;
 
         JsValue::from_serde(&requirements).map_err(|e| JsValue::from(e.to_string()))
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use wasm_bindgen_test::*;
+    const URL: &str = "ws://127.0.0.1:9944";
+
+    wasm_bindgen_test::wasm_bindgen_test_configure!(run_in_browser);
+
+    fn init_tracing() {
+        console_error_panic_hook::set_once();
+        tracing_wasm::set_as_global_default();
+    }
+
+    #[wasm_bindgen_test]
+    async fn test_query_chain() {
+        init_tracing();
+
+        let api = Api::from_url(URL).await.unwrap();
+
+        let chain = api.rpc().system_chain().await.unwrap();
+
+        assert_eq!(chain, "Development");
+    }
+
+    #[wasm_bindgen_test]
+    async fn test_query_members() {
+        let guild = "myguild".to_string();
+        let role = "".to_string();
+
+        let members_js = query_members(guild, role, URL.to_string()).await.unwrap();
+        let members_vec: Vec<gn_client::AccountId> = members_js.into_serde().unwrap();
+
+        assert_eq!(members_vec.len(), 6);
+    }
+
+    #[wasm_bindgen_test]
+    async fn test_query_guilds() {
+        let guild_name = "".to_string();
+        let guilds = query_guilds(guild_name, URL.to_string()).await.unwrap();
+        let guilds_vec: Vec<gn_client::data::GuildData> = guilds.into_serde().unwrap();
+
+        assert_eq!(guilds_vec.len(), 2);
+        assert_eq!(guilds_vec[0].roles.len(), 2);
+        assert_eq!(guilds_vec[1].roles.len(), 2);
+    }
+
+    #[wasm_bindgen_test]
+    async fn test_query_requirements() {
+        let guild_name = "myguild".to_string();
+        let role_name = "myrole".to_string();
+        let requirements_js = query_requirements(guild_name, role_name, URL.to_string())
+            .await
+            .unwrap();
+        let requirements: gn_common::requirements::RequirementsWithLogic =
+            requirements_js.into_serde().unwrap();
+
+        assert_eq!(requirements.logic, "0");
     }
 }
