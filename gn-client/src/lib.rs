d810897ecd73b110f32503be442343a576d9a69f
PopcornPaws <lipschitz77@gmail.com>
2022-12-09 17:27:38 +0100

    WIP wasm signature.

--- gn-client/src/lib.rs
+++ gn-client/src/lib.rs
@@ -20,22 +20,25 @@ pub mod queries;
 pub mod transactions;
 
 pub type AccountId = AccountId32;
+pub type SubstrateAddress = <ClientConfig as subxt::Config>::Address;
 pub type Api = OnlineClient<ClientConfig>;
 pub type BalanceOf = u128;
 pub type BlockHeader = Header<BlockNumber, BlakeTwo256>;
 pub type BlockNumber = u32;
 pub type BlockSubscription = Subscription<BlockHeader>;
 pub type GuildCall = runtime::runtime_types::pallet_guild::pallet::Call;
+pub type Index = u32;
 pub type OracleRequest = runtime::runtime_types::pallet_chainlink::pallet::GenericRequest<
     AccountId,
     GuildCall,
     u32,
-    u128,
+    BalanceOf,
 >;
 pub type Request = gn_common::Request<AccountId>;
 pub type RequestData = runtime::runtime_types::gn_common::RequestData;
 pub type RuntimeIdentity = runtime::runtime_types::gn_common::identities::Identity;
 pub type RuntimeIdentityWithAuth = runtime::runtime_types::gn_common::identities::IdentityWithAuth;
+pub type Signature = <ClientConfig as subxt::Config>::Signature;
 pub type Signer = PairSigner<ClientConfig, Keypair>;
 pub type TransactionProgress = TxProgress<ClientConfig, Api>;
 pub type TransactionStatus = SubTxStatus<ClientConfig, Api>;
--- gn-wasm/src/lib.rs
+++ gn-wasm/src/lib.rs
@@ -1,7 +1,120 @@
 use gn_client::queries::{self, GuildFilter};
-use gn_client::Api;
+use gn_client::{AccountId, Api, ClientConfig, Index, Signature, SubstrateAddress, TxSignerTrait};
 use gn_common::{pad::pad_to_32_bytes, GuildName};
+use js_sys::{Error, Function, JsString, Object, Promise};
 use wasm_bindgen::prelude::*;
+use web_sys::{console, window};
+
+use std::str::FromStr;
+
+fn get_sign_request(msg: &str, address: &str) -> Result<js_sys::Object, Error> {
+    let sign_request_param = js_sys::Object::new();
+
+    js_sys::Reflect::set(
+        &sign_request_param,
+        &JsString::from("address"),
+        &address.into(),
+    )?;
+    js_sys::Reflect::set(&sign_request_param, &JsString::from("data"), &msg.into())?;
+
+    Ok(sign_request_param)
+}
+
+pub struct WasmSigner {
+    pub lib: Object,
+    pub account_id: AccountId,
+    pub address: SubstrateAddress,
+}
+
+impl WasmSigner {
+    pub async fn new() -> Result<Self, Error> {
+        let window = window().expect("Failed to access window object");
+
+        let injected_web3 = window
+            .get("injectedWeb3")
+            .expect("Failed to access window.injectedWeb3");
+
+        let polkadot_js: JsValue =
+            js_sys::Reflect::get(&injected_web3, &JsString::from("polkadot-js"))?;
+
+        let enable: Function =
+            js_sys::Reflect::get(&polkadot_js, &JsString::from("enable"))?.into();
+
+        let lib: Object =
+            wasm_bindgen_futures::JsFuture::from(Promise::from(enable.call0(&JsValue::NULL)?))
+                .await?
+                .into();
+
+        let addresses: js_sys::Array = wasm_bindgen_futures::JsFuture::from(Promise::from(
+            js_sys::Function::from(js_sys::Reflect::get(
+                &js_sys::Reflect::get(&lib, &JsString::from("accounts"))?,
+                &JsString::from("get"),
+            )?)
+            .call0(&JsValue::NULL)?,
+        ))
+        .await?
+        .into();
+
+        let name: String = js_sys::Reflect::get(&addresses.at(0), &"name".into())?
+            .as_string()
+            .expect("Failed to cast addresses[0] to String");
+        let address: String = js_sys::Reflect::get(&addresses.at(0), &"address".into())?
+            .as_string()
+            .expect("Failed to cast addresses[0] to String");
+
+        console::log_1(&format!("Hello {}! ({})", name, address).into());
+
+        let account_id = AccountId::from_str(&address).expect("invalid address");
+        let address = SubstrateAddress::from(account_id.clone());
+
+        Ok(Self {
+            lib,
+            account_id, 
+            address,
+        })
+    }
+}
+
+impl TxSignerTrait<ClientConfig> for WasmSigner {
+    fn nonce(&self) -> Option<Index> {
+        Some(123)
+    }
+
+    fn account_id(&self) -> &AccountId {
+        &self.account_id
+    }
+
+    fn address(&self) -> SubstrateAddress {
+        self.address
+    }
+
+    fn sign(&self, signer_payload: &[u8]) -> Signature {
+        let signer =
+            js_sys::Reflect::get(&self.lib, &"signer".into()).expect("failed to get signer");
+        let sign_raw: Function = js_sys::Reflect::get(&signer, &"signRaw".into())
+            .expect("failed to get signature function")
+            .into();
+
+        let sign_payload =
+            get_sign_request(signer_payload, &self.address).expect("failed to get sign request");
+
+        sign_raw
+            .call1(&JsValue::NULL, &sign_payload)
+            .expect("failed to get sign promise")
+            .into()
+    }
+}
+
+#[wasm_bindgen(js_name = "polkadotSign")]
+pub async fn polkadot_sign(message: &str) -> Result<(), Error> {
+    console::log_2(&"Message to sign:".into(), &message.into());
+
+    let signer = WasmSigner::new().await?;
+    let signature = signer.sign(message.as_bytes());
+
+    console::log_1(&signature);
+    Ok(())
+}
 
 #[wasm_bindgen(js_name = "queryMembers")]
 pub async fn query_members(guild: String, role: String, url: String) -> Result<JsValue, JsValue> {
