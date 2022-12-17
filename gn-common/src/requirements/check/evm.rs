660e6a410c9ea311f555a02aeb634a8e39404230
richardanyalai <gtaricsi@gmail.com>
2022-12-17 22:27:24 +0100

    Fixed tests

--- gn-common/src/requirements/check/evm.rs
+++ gn-common/src/requirements/check/evm.rs
@@ -5,7 +5,7 @@ use providers::{evm::general::PROVIDERS, BalanceQuerier, EvmChain as RustyEvmCha
 use reqwest::Client as ReqwestClient;
 
 // only compute this once
-const MULTIPLIER: u128 = 1_000_000_000_000_000_000; // 10^18
+const MULTIPLIER: f64 = 1_000_000_000_000_000_000.0; // 10^18
 
 pub async fn get_balance(
     _client: &ReqwestClient,
@@ -42,6 +42,9 @@ pub async fn get_balance(
             address: token_address,
             id: token_id,
         }) => {
+            dbg!(Into::<providers::Address>::into(token_address));
+            dbg!(Into::<providers::U256>::into(token_id));
+
             provider
                 .get_non_fungible_balance(
                     token_address.into(),
@@ -59,7 +62,7 @@ pub async fn get_balance(
     //
     // we wouldn't need any of this if the get_balance would return and U256
     let balance = if let Some(Ok(balance)) = results.get(0) {
-        *balance as u128 * MULTIPLIER
+        (*balance * MULTIPLIER) as u128
     } else {
         0
     };
@@ -78,33 +81,34 @@ mod test {
         222, 78, 23, 156, 193, 211, 178, 152, 33, 107, 150, 137, 55, 103, 185, 176, 26, 107, 196,
         19,
     ];
+    // 0x57f1887a8bf19b14fc0df6fd9b2acc9af147ea85
+    const NFT_ADDRESS: EvmAddress = [
+        87, 241, 136, 122, 139, 241, 155, 20, 252, 13, 246, 253, 155, 42, 204, 154, 241, 71, 234,
+        133,
+    ];
     // 0xE43878Ce78934fe8007748FF481f03B8Ee3b97DE
     const USER_ADDRESS: EvmAddress = [
         228, 56, 120, 206, 120, 147, 79, 232, 0, 119, 72, 255, 72, 31, 3, 184, 238, 59, 151, 222,
     ];
-    // 0x535053a1cc874c9be92180e599c2529adfbd49f0
-    const NFT_ADDRESS: EvmAddress = [
-        83, 80, 83, 161, 204, 135, 76, 155, 233, 33, 128, 229, 153, 194, 82, 154, 223, 189, 73, 240,
-    ];
-    // 44533621599928789006513101770322670729981204560050458486968461274604483117072
+    // 61313325075603536901663283754390960556726744542208800735045237225934362163454
     const ID: U256 = [
-        16, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 220, 221, 62, 82, 235, 65, 53, 128, 171, 116, 103,
-        193, 128, 219, 39, 130, 247, 34, 117, 98,
+        135, 142, 28, 184, 229, 47, 185, 39, 243, 86, 182, 36, 52, 56, 187, 61, 223, 186, 163, 212,
+        169, 0, 39, 121, 10, 11, 108, 224, 183, 30, 184, 254,
     ];
-    // 10_000_000_000_000_000
+    // 10_000_000_000_000_000 (0.01)
     const BALANCE_E16: U256 = [
         0, 0, 193, 111, 242, 134, 35, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
         0, 0, 0, 0, 0,
     ];
-    // 1000
+    // 464_468_855_704_627 (0.000464468855704627)
     const BALANCE_E3: U256 = [
-        232, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0,
+        51, 76, 39, 149, 110, 166, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0,
     ];
-    // 100
+    // 1_000_000_000_000_000_000 (1.0)
     const BALANCE_E2: U256 = [
-        100, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0,
+        0, 0, 100, 167, 179, 182, 224, 13, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0,
     ];
 
     #[tokio::test]
@@ -112,7 +116,7 @@ mod test {
         let client = reqwest::Client::new();
 
         // check zero address
-        let balance = get_balance(&client, &None, &[0; 20], EvmChain::Ethereum)
+        let balance = get_balance(&client, &None, &USER_ADDRESS, EvmChain::Ethereum)
             .await
             .unwrap();
 
@@ -141,7 +145,7 @@ mod test {
             id: ID,
         });
 
-        let balance = get_balance(&client, &token_type, &USER_ADDRESS, EvmChain::Polygon)
+        let balance = get_balance(&client, &token_type, &USER_ADDRESS, EvmChain::Ethereum)
             .await
             .unwrap();
 
