04d23ee40cf766987f26e8066546dc0153f36881
PopcornPaws <lipschitz77@gmail.com>
2023-02-08 12:24:26 +0100

    Fixed queries without changing our wrappers.

--- gn-client/src/lib.rs
+++ gn-client/src/lib.rs
@@ -1,50 +1,48 @@
-use subxt::{
-    events::{EventSubscription, FilterEvents},
-    ext::sp_runtime::{generic::Header, traits::BlakeTwo256, AccountId32},
-    rpc::Subscription,
-    tx::{PairSigner, TxProgress, TxStatus as SubTxStatus},
-    OnlineClient,
-};
-
-// re-exports
+//    events::{EventSubscription, FilterEvents},
+//    ext::sp_runtime::{generic::Header, traits::BlakeTwo256, AccountId32},
+//    rpc::Subscription,
+//    tx::{PairSigner, TxProgress, TxStatus as SubTxStatus},
+//    OnlineClient,
+//};
+//
+//// re-exports
 pub use serde_cbor::{from_slice as cbor_deserialize, to_vec as cbor_serialize};
-pub use subxt::ext::sp_core::crypto::Pair as TraitPair;
-pub use subxt::ext::sp_core::sr25519::Pair as Keypair;
-pub use subxt::ext::sp_core::sr25519::Signature as SrSignature;
-pub use subxt::ext::sp_core::H256 as Hash;
-pub use subxt::tx::{PreparedMsgWithParams, Signer as TxSignerTrait};
+//pub use subxt::ext::sp_core::crypto::Pair as TraitPair;
+//pub use subxt::ext::sp_core::sr25519::Pair as Keypair;
+//pub use subxt::ext::sp_core::sr25519::Signature as SrSignature;
+pub use subxt::utils::H256 as Hash;
+//pub use subxt::tx::{PreparedMsgWithParams, Signer as TxSignerTrait};
 pub use subxt::PolkadotConfig as ClientConfig;
 
 #[subxt::subxt(runtime_metadata_path = "./artifacts/metadata.scale")]
 pub mod runtime {}
 pub mod data;
 pub mod queries;
-pub mod transactions;
-
-pub type AccountId = AccountId32;
-pub type SubstrateAddress = <ClientConfig as subxt::Config>::Address;
-pub type Api = OnlineClient<ClientConfig>;
-pub type BalanceOf = u128;
-pub type BlockHeader = Header<BlockNumber, BlakeTwo256>;
-pub type BlockNumber = u32;
-pub type BlockSubscription = Subscription<BlockHeader>;
-pub type GuildCall = runtime::runtime_types::pallet_guild::pallet::Call;
-pub type Index = u32;
-pub type OracleRequest = runtime::runtime_types::pallet_oracle::pallet::GenericRequest<
-    AccountId,
-    GuildCall,
-    u32,
-    BalanceOf,
->;
+//pub mod transactions;
+pub type Api = subxt::OnlineClient<ClientConfig>;
+pub type AccountId = subxt::utils::AccountId32;
+//pub type SubstrateAddress = <ClientConfig as subxt::Config>::Address;
+//pub type BalanceOf = u128;
+//pub type BlockHeader = Header<BlockNumber, BlakeTwo256>;
+//pub type BlockNumber = u32;
+//pub type BlockSubscription = Subscription<BlockHeader>;
+//pub type GuildCall = runtime::runtime_types::pallet_guild::pallet::Call;
+//pub type Index = u32;
+//pub type OracleRequest = runtime::runtime_types::pallet_oracle::pallet::GenericRequest<
+//    AccountId,
+//    GuildCall,
+//    u32,
+//    BalanceOf,
+//>;
 pub type Request = gn_common::Request<AccountId>;
-pub type RequestData = runtime::runtime_types::gn_common::RequestData<AccountId>;
+//pub type RequestData = runtime::runtime_types::gn_common::RequestData<AccountId>;
 pub type RuntimeIdentity = runtime::runtime_types::gn_common::identities::Identity;
-pub type RuntimeIdentityWithAuth = runtime::runtime_types::gn_common::identities::IdentityWithAuth;
-pub type Signature = <ClientConfig as subxt::Config>::Signature;
-pub type Signer = PairSigner<ClientConfig, Keypair>;
+//pub type RuntimeIdentityWithAuth = runtime::runtime_types::gn_common::identities::IdentityWithAuth;
+//pub type Signature = <ClientConfig as subxt::Config>::Signature;
+//pub type Signer = PairSigner<ClientConfig, Keypair>;
 pub type SubxtError = subxt::Error;
-pub type TransactionProgress = TxProgress<ClientConfig, Api>;
-pub type TransactionStatus = SubTxStatus<ClientConfig, Api>;
-
-pub type FilteredEvents<'a, T> =
-    FilterEvents<'a, EventSubscription<ClientConfig, Api, BlockSubscription>, ClientConfig, T>;
+//pub type TransactionProgress = TxProgress<ClientConfig, Api>;
+//pub type TransactionStatus = SubTxStatus<ClientConfig, Api>;
+//
+//pub type FilteredEvents<'a, T> =
+//    FilterEvents<'a, EventSubscription<ClientConfig, Api, BlockSubscription>, ClientConfig, T>;
