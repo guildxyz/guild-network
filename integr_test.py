6aa373d67c5441561296e8da86c280f7dac70bf6
PopcornPaws <lipschitz77@gmail.com>
2023-02-09 11:25:21 +0100

    no such stderr output that we are waiting for.

--- integr_test.py
+++ integr_test.py
@@ -12,7 +12,7 @@ def start_node():
 
     start = time.time()
     line = b""
-    while b"creating instance on iface" not in line:
+    while b"Running JSON-RPC" not in line:
         line = node.stderr.readline()
         if int(time.time() - start) == 10:
             print("Node startup timeout, exiting...")
