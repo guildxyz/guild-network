e3dbd429e7c78629a10b2decdda878e3a6bde883
PopcornPaws <lipschitz77@gmail.com>
2023-02-17 13:06:23 +0100

    WIP

--- gn-pallets/pallet-oracle/src/benchmarking.rs
+++ gn-pallets/pallet-oracle/src/benchmarking.rs
@@ -1,1 +1,49 @@
+use super::*;
+use crate::Pallet as Oracle;
 
+use frame_benchmarking::{benchmarks, whitelisted_caller};
+use frame_system::RawOrigin;
+
+benchmarks! {
+    register_operator {
+        let caller: T::AccountId = whitelisted_caller();
+    }: _(RawOrigin::Signed(caller))
+    verify {
+        assert_eq!(Oracle::<T>::operators()[0], caller);
+    }
+    deregister_operator {
+        let operator: T::AccountId = whitelisted_caller();
+        Oracle::<T>::register_operator(RawOrigin::Signed(operator.clone()).into())?;
+        assert_eq!(Oracle::<T>::operators()[0], caller);
+    }: _(RawOrigin::Signed(operator))
+    verify {
+    }
+    // TODO it's hard to benchmark these as they are taking dynamic inputs
+    // and they are coupled with other pallets
+    //initiate_request {
+    //    let caller: T::AccountId = whitelisted_caller();
+    //    let operator: T::AccountId = account("operator", 1, 123);
+
+    //    Oracle::<T>::register_operator(RawOrigin::Signed(operator.clone()).into())?;
+
+    //    let spec_index = vec![0; 5];
+    //    let data_version = 987_u64;
+    //    let data = ["this", "and", "that"].encode();
+    //    let fee = T::Currency::minimum_balance();
+    //}: _(RawOrigin::Signed(caller), operator, spec_index, data_version, data, fee,
+    //verify {
+    //}
+    //callback {
+    //    let b in 0..1000;
+
+    //    let caller: T::AccountId = whitelisted_caller();
+    //    let request_id = 128_u64;
+    //    let operator: T::AccountId = account("operator", 1, 123);
+
+    //    Oracle::<T>::register_operator(RawOrigin::Signed(operator.clone()).into())?;
+    //    // TODO should add an initiate request
+
+    //}: _(RawOrigin::Signed(caller), request_id, vec![0; b as usize])
+    //verify {
+    //}
+}
