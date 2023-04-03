3a2db69759c13a8d97a7e372bb15013cafc1f67a
PopcornPaws <lipschitz77@gmail.com>
2023-04-03 11:33:22 +0200

    Implemented eth signer on the client (subxt) side.

--- gn-api/examples/guild/eth.rs
+++ gn-api/examples/guild/eth.rs
@@ -1,22 +1,41 @@
-use ethers::signers::Signer as EthSignerT;
-use gn_api::{Api, ClientConfig};
+use ethers::core::k256::ecdsa::SigningKey;
+use ethers::signers::{LocalWallet, Signer as EthSignerT};
 use gn_api::tx::{self, Signer};
+use gn_api::{Api, ClientConfig};
+use sp_core::hashing::blake2_256;
+
+use subxt::utils::{MultiAddress, MultiSignature};
 
 use std::sync::Arc;
 
-struct EthSigner(ethers::signers::LocalWallet);
+pub struct EthSigner {
+    account_id: <ClientConfig as subxt::Config>::AccountId,
+    wallet: LocalWallet,
+}
+
+impl EthSigner {
+    pub fn from_seed(seed: [u8; 32]) -> Self {
+        let wallet = LocalWallet::from(SigningKey::from_bytes(&seed).unwrap());
+        let account_id =
+            <ClientConfig as subxt::Config>::AccountId::from(blake2_256(wallet.address().as_ref()));
+        Self { account_id, wallet }
+    }
+}
 
 impl subxt::tx::Signer<ClientConfig> for EthSigner {
     fn account_id(&self) -> &<ClientConfig as subxt::Config>::AccountId {
-        todo!()
+        &self.account_id
     }
 
     fn address(&self) -> <ClientConfig as subxt::Config>::Address {
-        todo!()
+        MultiAddress::Address20(self.wallet.address().into())
     }
 
     fn sign(&self, signer_payload: &[u8]) -> <ClientConfig as subxt::Config>::Signature {
-        todo!()
+        futures::executor::block_on(async move {
+            let signature = self.wallet.sign_message(signer_payload).await.unwrap();
+            MultiSignature::Ecdsa(signature.into())
+        })
     }
 }
 
