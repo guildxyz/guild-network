8218ba7cdad8b914aa2992bbbb2fb2286c1c4ee2
PopcornPaws <lipschitz77@gmail.com>
2023-04-18 15:53:35 +0200

    Fixed errors.

--- gn-sig/src/lib.rs
+++ gn-sig/src/lib.rs
@@ -112,7 +112,7 @@ impl IdentifyAccount for MultiSigner {
         match self {
             Self::Ed25519(who) => <[u8; 32]>::from(who).into(),
             Self::Sr25519(who) => <[u8; 32]>::from(who).into(),
-            Self::Ecdsa(who) => gn_common::utils::evm_to_account(who.as_ref()).into(),
+            Self::Ecdsa(who) => gn_common::utils::evm_to_account(who.into()).into(),
         }
     }
 }
