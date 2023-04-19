dfcb31801124704c919e8e592f474d475dca4cce
OnyxSkyscape <onyxskyscape@disroot.org>
2023-04-19 12:22:18 +0200

    Added trap signal to stress test

--- stress.sh
+++ stress.sh
@@ -1,5 +1,6 @@
-#!/bin/sh
-# USAGE: sh ./stress.sh [number of threads] [tps per thread] [number of tx per thread]
+#!/bin/bash
+# USAGE: bash ./stress.sh [number of threads] [tps per thread] [number of tx per thread]
+trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
 for i in $(seq 1 $1); do
     ./target/release/gn-cli -i 65.108.102.250 stress --seed "$(head -c 10 /dev/urandom | sha1sum | head -c 10)" --tps $2 -n $3 register-other & 
 done
