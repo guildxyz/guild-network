74f93fa5a5564d115b1972f6323a1f2fb1b88ecb
OnyxSkyscape <onyxskyscape@disroot.org>
2023-04-20 20:42:19 +0200

    broken: revamped stress test

--- gn-cli/src/stress.rs
+++ gn-cli/src/stress.rs
@@ -4,6 +4,8 @@ use futures::future::try_join_all;
 use gn_api::{tx, Api};
 use gn_common::identity::{Identity, IdentityWithAuth};
 use gn_common::pad::pad_to_n_bytes;
+use itertools::Itertools;
+use std::time::Duration;
 
 use std::fs::File;
 use std::io::Write;
@@ -25,12 +27,12 @@ pub fn generate_evm_addresses(output: PathBuf, num: usize, seed: &str) {
 
 pub async fn register_other_identity(api: Api, num: usize, seed: &str, tps: usize, id_index: u8) {
     let mut id = 0usize;
-    let mut seed_bytes = pad_to_n_bytes::<32, _>(seed);
     let mut index = 0usize;
-    for (i, chunk_start) in (0..num).step_by(tps).enumerate() {
-        let chunk = chunk_start..(chunk_start + tps).min(num);
+    let mut seed_bytes = pad_to_n_bytes::<32, _>(seed);
+
+    for chunk in &(0..num).chunks(tps) {
         let tx_futures = chunk
-            .map(|_| {
+            .map(|i| {
                 let password = format!("//{}", hex::encode(seed_bytes));
                 let signer = tx::signer("", Some(&password)).expect("invalid signer");
                 let identity = Identity::Other(pad_to_n_bytes::<64, _>(format!("other{}", id)));
@@ -39,15 +41,11 @@ pub async fn register_other_identity(api: Api, num: usize, seed: &str, tps: usiz
                 seed_bytes[index] = seed_bytes[index].wrapping_add(1);
                 index = index.wrapping_add(1) % seed_bytes.len();
                 let payload = tx::register(identity_with_auth, id_index);
+                log::info!("registration {} submitted", i);
                 tx::send::owned_but_dont_watch(api.clone(), payload, signer)
             })
             .collect::<Vec<_>>();
-
-        try_join_all(tx_futures)
-            .await
-            .expect("failed to send transactions");
-
-        tokio::time::sleep(std::time::Duration::from_millis(950)).await;
-        log::info!("registration batch {} submitted", i);
+        try_join_all(tx_futures).await.expect("failed to send tx");
+        tokio::time::sleep(Duration::from_secs(1)).await;
     }
 }
